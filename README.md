4a7b01b2814090f4c0d9fdb8f8b01836303fbd89
Paulo Lins <paulolins89@gmail.com>
2021-03-30 16:36:27 -0300

    started making intial things on calculator

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # calculator
+
+I will be making a calculator which will do simple operations and have an interfact that the user can interact with
---
+++ calculator.js
@@ -0,0 +1,5 @@
+//TODO Add function
+//TODO Subtract function
+//TODO Multiply function
+//TODO Divide function
+//TODO Operate function --> Takes two numbers and an operator and calls ones of the functions above
