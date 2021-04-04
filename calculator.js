70fa75bfc1b203b6fb2611fefdd4ecedb45bfff8
Paulo Lins <paulolins89@gmail.com>
2021-04-04 09:48:13 -0300

    added the negative button

--- calculator.js
+++ calculator.js
@@ -2,6 +2,9 @@
 
 const buttons = document.querySelectorAll('.button');
 const screen = document.querySelector('#screen');
+let firstNumber = 0;
+let secondNumber = 0;
+
 screen.textContent = '0';
 
 buttons.forEach((button) => {
@@ -11,21 +14,41 @@ buttons.forEach((button) => {
         if ((e.target.className == 'button number') && (screen.textContent == '0') && (e.target.id != 'point')){
             screen.textContent = e.target.value;
         //if the number on the screen is not 0, it appends the number to the end of the sequenece (there can only be 9 numbers max on screen)
-        }else if ((e.target.className == 'button number') && (screen.textContent != '0') && (screen.textContent.length < 9)){
+        }else if ((e.target.className == 'button number') && (screen.textContent != '0') && (screen.textContent.length < 8)){
             screen.textContent += e.target.value;
         //if you are clicking period and the number on the screen is 0, it changes the number to 0.
         }else if ((e.target.id == 'point') && (screen.textContent == '0')){
             screen.textContent = "0.";
         //adds period when clicked, but can only add it one time.
+        //the split function counts how many elements were made when you split the string at the period
+        //if the length is 2 (two parts) - 1 = 1, that means that there is already a period there
         }else if ((e.target.id == 'point') && ((screen.textContent.split('.').length - 1) < 1)){
             screen.textContent += e.target.value;
-        //returns everything back to 0 NEEDS TO CHANGE TO CLEAR VARIABLES
+        //returns everything back to 0 
+        //NEEDS TO BE ABLE TO CLEAR VARIABLES
         }else if (e.target.id == 'clear'){
             screen.textContent = '0';
+            firstNumber = 0;
+            secondNumber = 0;
         //deletes the last things added to the screen
         }else if (e.target.id == 'delete'){
             screen.textContent = screen.textContent.slice(0, -1);
+        //add the negative sign to the screen if it isn't there or removes it
+        }else if (e.target.id == 'negative'){
+            if (screen.textContent.slice(0,1) != '-'){
+                screen.textContent = '-' + screen.textContent;
+            }else{
+                screen.textContent = screen.textContent.slice(1);
+            }
+        //TODO - add function for percentage button
+        }else if (e.target.id == 'percent'){
+            
+        }else if (e.target.className == 'button operator'){
+            firstNumber = parseFloat(screen.textContent);
+            
         }
+        
+        //TODO - add activator for operator
 
         //if there is nothing on the screen, it puts 0 back
         if (screen.textContent == ''){
