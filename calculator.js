9425e912f475af21e010dfda8d4342e93889af77
Paulo Lins <paulolins89@gmail.com>
2021-04-05 21:45:23 -0300

    did a bunch of stuff, and forgot to commit lol

--- calculator.js
+++ calculator.js
@@ -1,35 +1,56 @@
-//TODO Click buttons and make them appear on screen
+add = (a, b) => (a + b);
+subtract = (a, b) => (a - b);
+multiply = (a, b) => (a * b);
+divide = (a, b) => (a / b);
+
+operate = (a, b, operator) => {
+    if (operator == 'add'){
+        return add(a,b);
+    }else if (operator == 'subtract'){
+        return subtract(a,b);
+    }else if (operator == 'multiply'){
+        return multiply(a,b);
+    }else if (operator == 'divide'){
+        return divide(a,b);
+    }
+}
 
 const buttons = document.querySelectorAll('.button');
 const screen = document.querySelector('#screen');
-let firstNumber = 0;
-let secondNumber = 0;
 
+let firstNumber;
+let secondNumber;
+let currentOperator;
+let newNumbersOnScreen = true;
 screen.textContent = '0';
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
 
         //if you are clicking a number (not including period) and the number on the screen is 0, it changes the number
-        if ((e.target.className == 'button number') && (screen.textContent == '0') && (e.target.id != 'point')){
+        if ((e.target.className == 'button number') && (newNumbersOnScreen == true) && (e.target.id != 'point')){
             screen.textContent = e.target.value;
+            newNumbersOnScreen = false;
         //if the number on the screen is not 0, it appends the number to the end of the sequenece (there can only be 9 numbers max on screen)
-        }else if ((e.target.className == 'button number') && (screen.textContent != '0') && (screen.textContent.length < 8)){
+        }else if ((e.target.className == 'button number') && (newNumbersOnScreen == false) && (screen.textContent.length < 8)){
             screen.textContent += e.target.value;
         //if you are clicking period and the number on the screen is 0, it changes the number to 0.
         }else if ((e.target.id == 'point') && (screen.textContent == '0')){
             screen.textContent = "0.";
+            newNumbersOnScreen = false;
         //adds period when clicked, but can only add it one time.
         //the split function counts how many elements were made when you split the string at the period
         //if the length is 2 (two parts) - 1 = 1, that means that there is already a period there
         }else if ((e.target.id == 'point') && ((screen.textContent.split('.').length - 1) < 1)){
             screen.textContent += e.target.value;
+            newNumbersOnScreen = false;
         //returns everything back to 0 
-        //NEEDS TO BE ABLE TO CLEAR VARIABLES
         }else if (e.target.id == 'clear'){
             screen.textContent = '0';
-            firstNumber = 0;
-            secondNumber = 0;
+            firstNumber = undefined;
+            secondNumber = undefined;
+            currentOperator = undefined;
+            newNumbersOnScreen = true;
         //deletes the last things added to the screen
         }else if (e.target.id == 'delete'){
             screen.textContent = screen.textContent.slice(0, -1);
@@ -44,8 +65,21 @@ buttons.forEach((button) => {
         }else if (e.target.id == 'percent'){
             
         }else if (e.target.className == 'button operator'){
-            firstNumber = parseFloat(screen.textContent);
-            
+            if (e.target.id != currentOperator){
+                if ((firstNumber == undefined) && (secondNumber == undefined)){
+                    firstNumber = parseFloat(screen.textContent);
+                }else if((firstNumber != undefined) && (secondNumber == undefined)){
+                    secondNumber = parseFloat(screen.textContent);
+                    screen.textContent = operate(firstNumber, secondNumber, currentOperator);
+                    console.log(firstNumber, secondNumber, currentOperator);                
+                    firstNumber = parseFloat(screen.textContent);
+                    secondNumber = undefined;
+                }
+                if (e.target.id != 'equals') {
+                    currentOperator = e.target.id;
+                }
+                newNumbersOnScreen = true;   
+            }
         }
         
         //TODO - add activator for operator
@@ -53,12 +87,13 @@ buttons.forEach((button) => {
         //if there is nothing on the screen, it puts 0 back
         if (screen.textContent == ''){
             screen.textContent = '0';
+        } else if (screen.textContent == 'Infinity'){
+            screen.textContent = 'ERROR'
         }
     });
 });
 
-//TODO Add function
-//TODO Subtract function
-//TODO Multiply function
-//TODO Divide function
+
+
+
 //TODO Operate function --> Takes two numbers and an operator and calls ones of the functions above
